f7b1c2cad01a8f6a7f7f9861cf58a345cfb23d00
Juan Gutierrez <juanny.gee@gmail.com>
2024-12-08 15:27:07 -0500

    Remove comments that now live in README

--- experiments/jobs.c
+++ experiments/jobs.c
@@ -1,69 +1,8 @@
 /**
-
-    Field           Received Format     Usage in Block Header
-    -----           ---------------     ---------------------
-    prevhash        Big-endian          Little-endian
-    coinbase1       Hex string          Used as-is
-    coinbase2       Hex string          Used as-is
-    merkle_branch   Big-endian          Big-endian (for Merkle root calculation)
-    version         Big-endian          Little-endian
-    nbits           Big-endian          Big-endian
-    ntime           Big-endian          Little-endian
-
- * Sample job message, for block 100,000
- * {
-   "id" : null,
-   "method" : "mining.notify",
-   "params" : [
-      // params[0] - job id
-      "674320f700005d59",
-
-      // params[1] - previous block hash (big-endian)
-      "000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250",
-
-      // NOTE: params [2] & [3] were decomposed from the coinbase tx of block 100,000
-      https://mempool.space/api/tx/8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87/hex
-
-      // params [2] - prefix of the coinbase transaction (to precede extra nonce 2).
-      "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff08044c86041b",
-
-      // NOTE: extra nonce 2 for coinbase tx of block 100,000 = 1b020602 (like coinbase1 & coinbase2, endianess does not
-apply)
-
-      // params [3] - suffix of the coinbase transaction (to follow extra nonce 2).
-      "ffffffff0100f2052a010000004341041b0e8c2567c12536aa13357b79a073dc4444acb83c4ec7a0e2f99dd7457516c5817242da796924ca4e99947d087fedf9ce467cb9f7c6287078f801df276fdf84ac00000000",
-
-      // NOTE: How did I come up with these merkle branches when block 100,000 had 4 transactions?
-      // 1) The first transaction we'll compute from double_sha of coinbase1_extranonce2_coinbase2
-      // 2) Generally mining.notify sends branches that would require the smallest number of computations. So given txs
-[TX1 (coinbase), TX2, TX3, TX4] yields Root
-         /    \
-       H12    H34
-       / \    / \
-      H1 H2  H3 H4
-
-      We only need [H2, H34] to reach the root. So the second element is the double_sha of tx3 & tx4 concatenated.
-      NOTE: H2 is actually reversed here (because it's usually little-endian). See:
-https://medium.com/@stolman.b/1-4-hashing-transactions-txids-to-find-the-merkle-root-3aa5255f8de8
-      // params[4] - array of merkle branches (big-endian).
-      [
-         "c40297f730dd7b5a99567eb8d27b78758f607507c52292d02d4031895b52f2ff",
-         "49aef42d78e3e9999c9e6ec9e1dddd6cb880bf3b076a03be1318ca789089308e"
-      ],
-
-      // params[5] - version (big-endian)
-      "00000001",
-
-      // params[6] - difficulty ("bits" field of block header; big-endian; this is an encoding:
-https://developer.bitcoin.org/reference/block_chain.html#target-nbits) "4c86041b",
-
-      // params[7] - current time for the block (big-endian - hex(1293623863)[2:])
-      "4d1b2237",
-      true
-   ]
-}
+ * How to compile and execute:
+ *
+ * gcc jobs.c -lssl -lcrypto && ./a.out
  */
-
 #include "utils.h"
 #include <errno.h>
 #include <stdbool.h>
@@ -74,18 +13,6 @@ https://developer.bitcoin.org/reference/block_chain.html#target-nbits) "4c86041b
 #include <string.h>
 
 // BIG TODO: uint8_t vs unsigned char type ???
-
-/**
- * https://reference.cash/mining/stratum-protocol
- * extranonce2 size is provided in stratum response to mining.subscribe notify
- * {"result":[[["mining.notify","<SESSION_ID>"]],"<EXTRANONCE_1>",<SIZEOF_EXTRANONCE_2>],"id":2,"error":null}
- * Example: {"result":[[["mining.notify","67f68537"]],"61dd9767",8],"id":2,"error":null}
- * "61dd9767" is the value of extranonce1 and extranonce2 has a size of 8 bytes
- *
- * I'm starting to see why kano has a `pool` object...holds data from startum responses that change somewhat
- * infrequently
- */
-
 struct mining_subscribe_response {
     char *extranonce1;
     uint8_t extranonce2_size;
@@ -176,14 +103,6 @@ unsigned char *build_merkle_root(unsigned char *cb_tx_id, unsigned char **merkle
 }
 
 int main() {
-    /**
-     * https://mempool.space/api/tx/8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87/hex
-     * Because block 100,000 is quite old, decomposing the serialized coinbase tx, we see that
-     * extranonce1 ==   0x02 and is only 1 byte
-     * extranonce2 == 0x0602 and is only 2 bytes
-     *
-     * extranonce1 is generally 4 to 8 bytes
-     */
     debug_msg("building mining_subscribe_response");
     struct mining_subscribe_response sub;
     sub.extranonce1 = "02\0";
