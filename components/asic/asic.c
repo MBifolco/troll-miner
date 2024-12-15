e1b8644c0929eab12bd5eff1e7a16fe0024088cf
Juan Gutierrez <juanny.gee@gmail.com>
2024-12-15 12:28:50 -0500

    Build block header

--- components/asic/asic.c
+++ components/asic/asic.c
@@ -14,12 +14,36 @@
 static const char *TAG = "ASIC";
 
 void log_job(struct job *j) {
-    ESP_LOGI(TAG, "0x%lx", j->version);
+    ESP_LOGI(TAG, "version: 0x%lx", j->version);
     print_hex(j->previous_block_hash, HASH_SIZE, 160, "PrevBlockHash: ");
     print_hex(j->merkle_tree_root, HASH_SIZE, 160, "MerkleTreeRoot: ");
-    ESP_LOGI(TAG, "0x%lx", j->time);
-    ESP_LOGI(TAG, "0x%lx", j->nbits);
-    ESP_LOGI(TAG, "0x%lx", (uint32_t)0);
+    ESP_LOGI(TAG, "time: 0x%lx", j->time);
+    ESP_LOGI(TAG, "nbits: 0x%lx", j->nbits);
+    ESP_LOGI(TAG, "nonce: 0x%lx", (uint32_t)0);
+}
+
+void copy_4bytes_to_block_header(uint8_t *bh, uint32_t n, uint8_t idx) {
+    bh[idx] = (n >> 24) & 0xFF; // Most significant byte
+    bh[idx + 1] = (n >> 16) & 0xFF;
+    bh[idx + 2] = (n >> 8) & 0xFF;
+    bh[idx + 3] = n & 0xFF; // Least significant byte
+}
+
+void build_block_header(struct job *j) {
+    uint8_t block_header[80];
+    uint8_t previous_block_hash_offset = 4;
+    uint8_t merkle_tree_root_offest = previous_block_hash_offset + HASH_SIZE;
+    uint8_t time_offset = merkle_tree_root_offest + HASH_SIZE;
+    uint8_t nbits_offset = time_offset + 4;
+    uint8_t nonce_offset = nbits_offset + 4;
+
+    copy_4bytes_to_block_header(block_header, j->version, 0);
+    memcpy(block_header + previous_block_hash_offset, j->previous_block_hash, HASH_SIZE);
+    memcpy(block_header + merkle_tree_root_offest, j->merkle_tree_root, HASH_SIZE);
+    copy_4bytes_to_block_header(block_header, j->time, time_offset);
+    copy_4bytes_to_block_header(block_header, j->nbits, nbits_offset);
+    copy_4bytes_to_block_header(block_header, (uint32_t)0x0f2b5710, nonce_offset); // TODO: Should start @ 0
+    print_hex(block_header, 80, 160, "Block Header: ");
 }
 
 void asic_task(void *pvParameters) {
@@ -32,6 +56,7 @@ void asic_task(void *pvParameters) {
         if (xQueueReceive(work_to_asic_queue, &j, portMAX_DELAY) == pdPASS) {
             ESP_LOGI(TAG, "Received work from queue: %p", j);
             log_job(j);
+            build_block_header(j);
             free(j->extranonce2);
             free(j);
         } else {
