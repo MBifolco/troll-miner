a4780ad14df4644f4e6acb430524696d92c0e213
Juan Gutierrez <juanny.gee@gmail.com>
2024-12-20 16:58:17 -0500

    Provide a 32-bit equivalent for target & mask transform

--- experiments/target_v_difficulty.c
+++ experiments/target_v_difficulty.c
@@ -14,10 +14,43 @@
  */
 static const double truediffone = 26959535291011309493156476344723991336010898738574164086137773096960.0;
 static const double bits192 = 6277101735386680763835789423207666416102355444464034512896.0;
+static const double bits160 = 1461501637330902918203684832716283019655932542976.0;
 static const double bits128 = 340282366920938463463374607431768211456.0;
+static const double bits96 = 79228162514264337593543950336.0;
 static const double bits64 = 18446744073709551616.0;
+static const double bits32 = 4294967296.0;
 
-void calc_target(uint8_t *target, double diff) {
+void calc_target32(uint8_t *target, double diff) {
+    uint32_t *data32, h32;
+    double d32, dcut32;
+    /**
+     * We have to start from bits192, because there truediffone is 0x00000000FFFF....
+     * meaning there's already an offset - so to do the 32bit equivalent operations of
+     * target calc, we start at bits192, offset 24 in target, to account for the first
+     * 4 bytes of truediffone being 0. After that, we can proceed with 32 bit segments.
+     */
+    double segments[6] = {bits192, bits160, bits128, bits96, bits64, bits32};
+    uint8_t segment_offsets_in_target[6] = {24, 20, 16, 12, 8, 4};
+
+    d32 = truediffone;
+    d32 /= diff;
+
+    for (int i = 0; i < 6; i++) {
+        dcut32 = d32 / segments[i];
+        h32 = dcut32;
+        data32 = (uint32_t *)(target + segment_offsets_in_target[i]);
+        *data32 = htole32(h32);
+        dcut32 = h32;
+        dcut32 *= segments[i];
+        d32 -= dcut32;
+    }
+
+    h32 = d32;
+    data32 = (uint32_t *)(target);
+    *data32 = htole32(h32);
+}
+
+void calc_target64(uint8_t *target, double diff) {
     uint64_t *data64, h64;
     double d64, dcut64;
 
@@ -26,7 +59,6 @@ void calc_target(uint8_t *target, double diff) {
 
     dcut64 = d64 / bits192;
     h64 = dcut64;
-
     data64 = (uint64_t *)(target + 24);
     *data64 = htole64(h64);
     dcut64 = h64;
@@ -83,8 +115,16 @@ int main(int argc, char *argv[]) {
     }
 
     uint8_t target[32];
-    calc_target(target, diff);
-    printf("Target:\t");
+
+    calc_target32(target, diff);
+    printf("Target (32bit):\t");
+    for (int i = 31; i >= 0; i--) {
+        printf("%02x", target[i]);
+    }
+    printf("\n");
+
+    calc_target64(target, diff);
+    printf("Target (64bit):\t");
     for (int i = 31; i >= 0; i--) {
         printf("%02x", target[i]);
     }
@@ -92,7 +132,7 @@ int main(int argc, char *argv[]) {
 
     uint8_t mask[32];
     calc_mask(mask, target);
-    printf("Mask:\t");
+    printf("Mask:\t\t");
     for (int i = 31; i >= 0; i--) {
         printf("%02x", mask[i]);
     }
